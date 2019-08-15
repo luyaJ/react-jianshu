01b3bb9d2ac419916c44027e563026003e61fb6d
luyaj <1579618152@qq.com>
2019-08-15 14:07:45 +0800

    redux-immutable

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,20 +1,18 @@
 import * as constants from './constants';
+import { fromJS } from 'immutable';
 
-const defaultState = {
+const defaultState = fromJS({
   focused: false
-};
+});
 
 // 纯函数：给定固定的输入就有固定的输出，无副作用
-export default (state=defaultState, action) => {
+export default (state = defaultState, action) => {
   if(action.type === constants.SEARCH_FOCUS) {
-    return {
-      focused: true
-    }
+    // immutable对象的set方法 会结合之前immutable对象的值和设置的值，返回一个全新的对象
+    return state.set('focused', true);
   }
   if(action.type === constants.SEARCH_BLUR) {
-    return {
-      focused: false
-    }
+    return state.set('focused', false);
   }
   return state;
 }
